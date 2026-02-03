4a1f839313ab4c1f317c8df76a5db853fcfe67b4
Rafaelly <rafaellypoliveira@gmail.com>
2026-02-02 21:12:04 -0300

    feat: add mutex-protected logging function for thread-safe output

--- src/routine.c
+++ src/routine.c
@@ -3,37 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 23:59:46 by codespace         #+#    #+#             */
-/*   Updated: 2026/01/26 01:10:30 by codespace        ###   ########.fr       */
+/*   Updated: 2026/02/02 20:57:11 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <stdio.h>
 
-long long   timestamp(void);
-void        philo_take_forks(t_rules *rules, t_philo *philo);
-void        philo_eat(t_rules *rules, t_philo *philo);
-void        philo_drop_forks(t_rules *rules, t_philo *philo);
-void        philo_sleep(t_rules *rules, t_philo *philo);
+void	philo_take_forks(t_philo *philo);
+void	philo_eat(t_philo *philo);
+void	philo_drop_forks(t_philo *philo);
+void	philo_sleep(t_philo *philo);
+void	print_log(t_philo *philo, char *s);
 
-void    philo_routine(t_rules *rules, t_philo *philo)
+void	philo_routine(t_philo *philo)
 {
-    long long   time;
-    
-    time = timestamp();
-    if (!time)
-        return ;
-    while ()
-    {
-        pthread_mutex_lock(&rules->print_mutex);
-        printf("%lld %d is thinking.\n", time, philo->philo_id);
-        pthread_mutex_unlock(&rules->print_mutex);
-        philo_take_forks(rules, philo);
-        philo_eat(rules, philo);
-        philo_drop_forks(rules, philo);
-        philo_sleep(rules, philo);
-    }
+	long long	time;
+
+	time = timestamp(philo);
+	if (!time)
+		return ;
+	while (1)
+	{
+		print_log(philo, "is thinking");
+		philo_take_forks(philo);
+		philo_eat(philo);
+		philo_drop_forks(philo);
+		philo_sleep(philo);
+	}
 }
