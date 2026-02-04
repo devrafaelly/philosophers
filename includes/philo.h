195159c5eb9e980a05f36f18dd6ec7345bc8b125
Rafaelly <rafaellypoliveira@gmail.com>
2026-02-03 21:56:05 -0300

    refactor: replace rules system with centralized data struct

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: devrafaelly <devrafaelly@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 02:55:03 by devrafaelly       #+#    #+#             */
-/*   Updated: 2026/02/02 20:52:56 by devrafaelly      ###   ########.fr       */
+/*   Updated: 2026/02/03 21:09:07 by devrafaelly      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,38 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-// memset, printf, malloc, free, write,
-// usleep, gettimeofday, pthread_create,
-// pthread_detach, pthread_join, pthread_mutex_init,
-// pthread_mutex_destroy, pthread_mutex_lock,
-// pthread_mutex_unlock
+typedef struct s_philo	t_philo;
 
-typedef struct s_rules
+typedef struct s_data
 {
-	int				number_of_philosophers;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				number_of_times_each_philosopher_must_eat;
+	int				n_philo;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				n_t_must_eat;
+	int				stop_flag;
 	long long		start_time;
-	pthread_mutex_t	log;
 	pthread_mutex_t	*forks;
-}	t_rules;
+	pthread_mutex_t	log;
+	pthread_mutex_t	init_time;
+	pthread_mutex_t	death;
+	t_philo			*philo;
+}	t_data;
 
 typedef struct s_philo
 {
 	int				philo_id;
 	long long		last_meal;
 	pthread_mutex_t	meal;
-	t_rules			*rules;
+	t_data			*data;
 }	t_philo;
 
 int			*parse_args(int ac, char **av);
-t_rules		*init_program(int ac, int *n);
-void		philo_routine(t_philo *philo);
-void		philo_init(t_rules *rules);
-long long	timestamp(t_philo *philo);
+t_data		*data_init(int ac, int *n);
+void		*philo_routine(void *arg);
+void		*monitor_routine(void *arg);
+void		thread_init(t_data *data);
+long long	timestamp(t_data *data);
+void		data_cleanup(t_data *data);
 
 #endif
